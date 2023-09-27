f4367f39ec91fcb1be8b4c35822229f298e8f17f
MateusAugusro <mateusaugusto2017pp@gmail.com>
2023-09-27 20:04:28 -0300

    start project flutter

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'screens/telaDeEntrada.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: TelaDeEntrada(),
+  ));
+}
